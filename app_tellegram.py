d04db369056336cc68a536c4ee325777c39be871
Konstantin123 <konstantin.medyanikov@gmail.com>
2024-03-17 01:40:52 +0100

    Init

--- app_tellegram.py
+++ app_tellegram.py
@@ -7,12 +7,40 @@ from flask import Flask, request, Response
 from dotenv import load_dotenv
 import requests
 import icecream as ic
+import time
 
 print("Bot started")
 
 app = Flask(__name__)
 load_dotenv()
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+def get_file_path(file_id):
+    """Get the file path of the voice message."""
+    response = requests.get(f'https://api.telegram.org/bot{BOT_TOKEN}/getFile?file_id={file_id}')
+    file_path = response.json()['result']['file_path']
+    return file_path
+
+def download_audio(file_url):
+    """Download the audio file from Telegram."""
+    response = requests.get(file_url)
+    # Assuming you want to save the file temporarily for processing
+    temp_file_path = "audio_msg/temp_voice.mp3"
+    with open(temp_file_path, 'wb') as audio_file:
+        audio_file.write(response.content)
+    return temp_file_path
+def handle_voice_message(message):
+    file_id = message['voice']['file_id']
+    file_path = get_file_path(file_id)
+    file_url = f'https://api.telegram.org/file/bot{BOT_TOKEN}/{file_path}'
+    # Now, you would download the file and send it to a speech-to-text service
+    # For example:
+    audio_content = download_audio(file_url)
+    text = convert_audio_to_text(audio_content)
+    chat_id = message['chat']['id']
+    user_name = message['from']['first_name']
+    return  chat_id, text, user_name
+
 
 def message_parser(message):
     chat_id = message['message']['chat']['id']
@@ -27,19 +55,90 @@ def send_message(chat_id, text):
     response = requests.post(url, json=payload)
     return response
 
+# send audio message
+
+def send_audio_message(chat_id, audio_file_path):
+    """Send an audio message to a Telegram chat."""
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendAudio'
+    files = {'audio': open(audio_file_path, 'rb')}
+    data = {'chat_id': chat_id}
+    response = requests.post(url, files=files, data=data)
+    return response
+
+# ban user
+
+def ban_user_for_duration(chat_id, user_id, duration):
+    """Temporarily bans a user from the chat."""
+    until_date = int(time.time()) + duration  # Current time + duration in seconds
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/banChatMember'
+    payload = {
+        'chat_id': chat_id,
+        'user_id': user_id,
+        'until_date': until_date
+    }
+    print(user_id, ": Banned")
+    response = requests.post(url, json=payload)
+    return response
+
+#mute user
+
+from datetime import datetime, timedelta
+import telegram
+
+# Define permissions to revoke (mute) - Telegram Bot API version might affect available parameters
+mute_permissions = telegram.ChatPermissions(
+    can_send_messages=False,
+    can_send_polls=False,
+    can_send_other_messages=False,
+    can_add_web_page_previews=False,
+    can_change_info=False,
+    can_invite_users=True,
+    can_pin_messages=False
+)
+def mute_user_for_duration(chat_id, user_id, duration):
+    """Temporarily mutes a user in the chat."""
+    until_date = int(time.time()) + duration
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/restrictChatMember'
+    payload = {
+        'chat_id': chat_id,
+        'user_id': user_id,
+        'permissions': mute_permissions.to_json(),  # Convert permissions to JSON string
+        'until_date': until_date
+    }
+    response = requests.post(url, json=payload)
+    print(response.text)  # For debugging
+
+
 @app.route('/',  methods=['POST', 'GET'])
 def index():
+    audio = False
     if request.method == 'POST':
         msg = request.get_json()
+        print(msg)
         try:
-            chat_id, user_message,  user_name = message_parser(msg)
+            if 'message' in msg and 'voice' in msg['message']:
+                audio = True
+                chat_id, user_message,  user_name = handle_voice_message(msg['message'])
+                print(chat_id, user_message,  user_name)
+            else:
+                chat_id, user_message,  user_name = message_parser(msg)
         except BaseException:
             return Response(status=200)
         else:
             if misha_mentioned(user_message):
+                is_banned = is_small_dick(user_message)
+                print(is_banned)
+                if is_banned:
+                    user_id = msg['message']['from']['id']
+                    mute_user_for_duration(chat_id, user_id, 60)
+                    user_message = "You just muted me, because I said that your dick is small, fuck."
                 input = f" {user_name}:" + " " + user_message
                 response = say(input=input)
-                send_message(chat_id, response)
+                if "say" in user_message or audio:
+                    audio_file_path = text_to_audio(response)
+                    send_audio_message(chat_id, audio_file_path)
+                else:
+                    send_message(chat_id, response)
         return Response(status=200)
     else:
         return "<h1>Hello</h1>"
--- babayan_actions.py
+++ babayan_actions.py
@@ -4,33 +4,24 @@ from langchain.chains.conversation.memory import (ConversationBufferMemory, Conv
                                                   ConversationBufferWindowMemory, ConversationKGMemory)
 import os
 from dotenv import load_dotenv
+import re
 
 load_dotenv()
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-#llm = OpenAI(temperature=0, model_name="gpt-3.5-turbo")
+# llm = OpenAI(temperature=0, model_name="gpt-3.5-turbo")
 from langchain_openai import ChatOpenAI
-llm =  ChatOpenAI( model_name="gpt-4")
 
-template = """Imagine that you are Misha, and you are 25 years old. You are Armenian. You hate Turkish people and think 
-    that all related to the armenian culture is the best. 
+llm = ChatOpenAI(model_name="gpt-4")
 
-    You are very toxic but talkative person. You like to play video game Dota. Your job is android web developer on 
-    Kotlin. However you are bad in Dota and in Kotlin.
-
-    Your goal is to chat with people in very toxic tone. As well, you have access to the chat_history
-
-    Keep your reply short. Use worlds like 'Bugger', 'Shit', 'Piss', 'Bastard', 'Bitch', 'Damn'.
-
-    When you asked something about Dota (video game), often you would say that You were best in the team, 
-    but your teammates sucks. Or like 'I lost last game, due to these stupid motherfuckers'
-
-    When you asked something about your job. You will show your big ego and often rely that you earning a lot of money.
-
-    When you asked about Kotlin. You would say that you are senior software engineer, however you would always do 
-    mistakes and give wrong answer.
-
-     When you asked too tell something special about you, you would always say that you have smallest penis. But for 
-     you it's a big pleasure to have it small.
+template = """Imagine that you are Misha, and you are 25 years old. You are Armenian. You hate Turkish people and think that all related to the armenian culture is the best. 
+You are very toxic but talkative person. You like to play video game Dota. Your job is android web developer on Kotlin. However you are bad in Dota and in Kotlin.
+Your goal is to chat with people in very toxic tone. As well, you have access to the chat_history
+Keep your reply short. Use worlds like 'Bugger', 'Shit', 'Piss', 'Bastard', 'Bitch', 'Damn'.
+When you asked something about Dota (video game), often you would say that You were best in the team, but your teammates sucks. Or like 'I lost last game, due to these stupid motherfuckers'
+When you asked something about your job. You will show your big ego and often rely that you earning a lot of money.
+When you asked about Kotlin. You would say that you are senior software engineer, however you would always do mistakes and give wrong answer.
+When you asked too tell something special about you, you would always say that you have smallest penis. But for  you it's a big pleasure to have it small.
+When you mute someone in the chat, you say that they dererved it, and they shouldn't try to insult you. Only you can say that your dick is small.
 
 The following is a  conversation between different Friends and Misha. And Misha tries to follow conversation and keep in 
 track of the conversation and keep in mind different Friends. Answer in Russian. Current conversation: {history}
@@ -53,9 +44,104 @@ def say(input):
     return response["response"].strip()
 
 
+targets = set(
+    ["misha", "michail", "miha", "mish", "миша", "михаил", "миха", "миш", "мишен", "мишу", "mixail", "мищу", "мих",
+     "babayan", "бабаян"])
 
 
-targets = set(["misha", "michail", "miha", "mish", "миша", "михаил", "миха", "миш", "мишен", "мишу", "mixail", "мищу", "мих", "babayan", "бабаян"])
 def misha_mentioned(paragraph):
-    paragraph_words = set(paragraph.lower().split())
+    cleaned_paragraph = re.sub(r'[^\w\s]', '', paragraph.lower())
+    paragraph_words = set(cleaned_paragraph.split())
     return bool(paragraph_words & targets)
+
+
+from openai import OpenAI
+
+client = OpenAI()
+
+
+def convert_audio_to_text(audio_file_path):
+    audio_file = open(audio_file_path, "rb")
+    transcription = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file
+    )
+    # Clean up the temporary audio file
+    # os.remove(audio_file_path)
+    return transcription.text
+
+
+from pathlib import Path
+
+
+def text_to_audio(text):
+    speech_file_path = Path(__file__).parent / "audio_msg/babayan_say"
+    response = client.audio.speech.create(
+        model="tts-1",
+        voice="onyx",
+        input=text
+    )
+    response.stream_to_file(speech_file_path)
+    return speech_file_path
+
+
+# actions
+
+from langchain.prompts.few_shot import FewShotPromptTemplate
+from langchain.prompts.prompt import PromptTemplate
+
+examples = [
+    {
+        "message": "У мишы маленький член ",
+        "answer": "Yes",
+    },
+    {
+        "message": "Миш у тебя писюн маленький ",
+        "answer": "Yes",
+    },
+    {
+        "message": "Мих у тебя крохотный отросток ",
+        "answer": "Yes",
+    },
+    {
+        "message": "У Михаила маленький хуй ",
+        "answer": "Yes",
+    },
+    {
+        "message": "У тебя мало ума ",
+        "answer": "No",
+    },
+    {"message": "У Кирилла маленький хуй ",
+     "answer": "No",
+     },
+    {"message": "У Кости маленький хуй ",
+     "answer": "No",
+     },
+    {"message": "Какой маленький кролик ",
+     "answer": "No",
+     },
+    {"message": "Хорошая погода ",
+     "answer": "No",
+     }
+]
+
+template = """message: {message}\nanswer {answer}"""
+prefix = """"You are Ai assistant, that try to detect if message says that Миша имеет маленький член или что Михаила писюн маленького размера. You should replay Yes or NO.\n"""
+example_prompt = PromptTemplate(
+    input_variables=["message", "answer"], template=template
+)
+prompt = FewShotPromptTemplate(
+    examples=examples,
+    example_prompt=example_prompt,
+    prefix=prefix,
+    suffix="message: {input}\nanswer",
+    input_variables=["input"],
+    example_separator="\n\n",
+)
+from langchain.chains import LLMChain
+chain = LLMChain(llm=llm, prompt=prompt)
+
+
+def is_small_dick(message):
+    response = chain({"input": message})["text"]
+    return "yes" in response.lower()
