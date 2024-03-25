72b8b2541074e8d0921301ef9a023d58b21ddea2
Konstantin123 <konstantin.medyanikov@gmail.com>
2024-03-25 01:53:33 +0100

    Init

--- app_tellegram.py
+++ app_tellegram.py
@@ -9,17 +9,23 @@ import requests
 import icecream as ic
 import time
 
+BABAYAN_ID = 1414008992
+
 print("Bot started")
+
+Black_List = {}
 
 app = Flask(__name__)
 load_dotenv()
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
 
 def get_file_path(file_id):
     """Get the file path of the voice message."""
     response = requests.get(f'https://api.telegram.org/bot{BOT_TOKEN}/getFile?file_id={file_id}')
     file_path = response.json()['result']['file_path']
     return file_path
+
 
 def download_audio(file_url):
     """Download the audio file from Telegram."""
@@ -29,6 +35,8 @@ def download_audio(file_url):
     with open(temp_file_path, 'wb') as audio_file:
         audio_file.write(response.content)
     return temp_file_path
+
+
 def handle_voice_message(message):
     file_id = message['voice']['file_id']
     file_path = get_file_path(file_id)
@@ -39,7 +47,7 @@ def handle_voice_message(message):
     text = convert_audio_to_text(audio_content)
     chat_id = message['chat']['id']
     user_name = message['from']['first_name']
-    return  chat_id, text, user_name
+    return chat_id, text, user_name
 
 
 def message_parser(message):
@@ -48,13 +56,28 @@ def message_parser(message):
     user_name = message['message']['from']['first_name']
     return chat_id, text, user_name
 
+
 # to send a message
 def send_message(chat_id, text):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    payload = {"chat_id": chat_id, "text": text}
+    payload = {"chat_id": chat_id, "text": text, "parse_mode": 'Markdown'}
+    response = requests.post(url, json=payload)
+    return response
+def message_update(chat_id, text, msg_id):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/editMessageText"
+    payload = {'chat_id': chat_id, 'message_id': msg_id, 'text': text, "parse_mode": 'Markdown'}
     response = requests.post(url, json=payload)
     return response
 
+def send_stream_data(chat_id, user_message, msg_id):
+    new_msg = " "
+    i = 0
+    for chunks in Ollama.stream(user_message):
+        new_msg = new_msg + chunks
+        if i%50==0:
+            message_update(chat_id, new_msg, msg_id)
+        i+=1
+        message_update(chat_id, new_msg, msg_id)
 # send audio message
 
 def send_audio_message(chat_id, audio_file_path):
@@ -65,8 +88,8 @@ def send_audio_message(chat_id, audio_file_path):
     response = requests.post(url, files=files, data=data)
     return response
 
-# ban user
 
+# ban user
 def ban_user_for_duration(chat_id, user_id, duration):
     """Temporarily bans a user from the chat."""
     until_date = int(time.time()) + duration  # Current time + duration in seconds
@@ -76,13 +99,12 @@ def ban_user_for_duration(chat_id, user_id, duration):
         'user_id': user_id,
         'until_date': until_date
     }
-    print(user_id, ": Banned")
     response = requests.post(url, json=payload)
+    print(response)
     return response
 
-#mute user
 
-from datetime import datetime, timedelta
+# mute user
 import telegram
 
 # Define permissions to revoke (mute) - Telegram Bot API version might affect available parameters
@@ -95,6 +117,8 @@ mute_permissions = telegram.ChatPermissions(
     can_invite_users=True,
     can_pin_messages=False
 )
+
+
 def mute_user_for_duration(chat_id, user_id, duration):
     """Temporarily mutes a user in the chat."""
     until_date = int(time.time()) + duration
@@ -106,43 +130,82 @@ def mute_user_for_duration(chat_id, user_id, duration):
         'until_date': until_date
     }
     response = requests.post(url, json=payload)
-    print(response.text)  # For debugging
+    print(response)  # For debugging
 
 
-@app.route('/',  methods=['POST', 'GET'])
+def ready_to_get_banned(user_id, max_limit=3):
+    BAN = False
+    key = str(user_id)
+    Black_List[key] += 1
+    if Black_List[key] >= max_limit:
+        BAN = True
+        Black_List[key] = 0
+    return BAN
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
     audio = False
     if request.method == 'POST':
         msg = request.get_json()
-        print(msg)
         try:
+            user_id = msg['message']['from']['id']
+            if not str(user_id) in Black_List:
+                Black_List[str(user_id)] = 0
             if 'message' in msg and 'voice' in msg['message']:
                 audio = True
-                chat_id, user_message,  user_name = handle_voice_message(msg['message'])
-                print(chat_id, user_message,  user_name)
+                chat_id, user_message, user_name = handle_voice_message(msg['message'])
             else:
-                chat_id, user_message,  user_name = message_parser(msg)
+                chat_id, user_message, user_name = message_parser(msg)
         except BaseException:
             return Response(status=200)
         else:
             if misha_mentioned(user_message):
-                is_banned = is_small_dick(user_message)
-                print(is_banned)
-                if is_banned:
-                    user_id = msg['message']['from']['id']
-                    mute_user_for_duration(chat_id, user_id, 60)
-                    user_message = "You just muted me, because I said that your dick is small, fuck."
-                input = f" {user_name}:" + " " + user_message
+                suffix = ""
+                small_dick_mentioned = is_small_dick(user_message)
+                if small_dick_mentioned:
+                    is_banned = ready_to_get_banned(user_id)
+                    if is_banned:
+                        ban_user_for_duration(chat_id, user_id, 60)
+                        suffix = "You just banned me, because I said that your dick is small, fuck."
+                        print(user_id, ": Banned")
+                    else:
+
+                        muted_time = Black_List[str(user_id)] * 60
+                        mute_user_for_duration(chat_id, user_id, muted_time)
+                        suffix = (f"You just muted me for {muted_time} seconds, because I said that your dick is "
+                                  f"small, fuck.")
+                        print(user_id, ": Muted for ", muted_time, " seconds")
+
+                prefix = f" {user_name} say:"
+                if user_id == BABAYAN_ID:
+                    prefix = f" Babayan (Misha's good friend) say:"
+                elif Black_List[str(user_id)] == 2:
+                    prefix = f" {user_name} ( Has Karma count = 2 ) say:"
+                input = prefix + " " + user_message + " " + suffix
                 response = say(input=input)
+                print(Black_List)
                 if "say" in user_message or audio:
                     audio_file_path = text_to_audio(response)
                     send_audio_message(chat_id, audio_file_path)
                 else:
                     send_message(chat_id, response)
+            elif "/code" in user_message:
+                response = send_message(chat_id, " ...")
+                msg_id = response.json()["result"]["message_id"]
+                response = Ollama.invoke(user_message)
+                message_update(chat_id, response, msg_id)
+                """response = send_message(chat_id, " ...")
+                msg_id = response.json()["result"]["message_id"]
+                send_stream_data(chat_id, user_message, msg_id)"""
+
+
+
+
         return Response(status=200)
     else:
         return "<h1>Hello</h1>"
 
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0" , debug=False, port=5000)
+    app.run(host="0.0.0.0", debug=False, port=5000)
--- babayan_actions.py
+++ babayan_actions.py
@@ -1,52 +1,81 @@
 from langchain.prompts.prompt import PromptTemplate
 from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import (ConversationBufferMemory, ConversationSummaryMemory,
-                                                  ConversationBufferWindowMemory, ConversationKGMemory)
+from langchain.chains.conversation.memory import (CombinedMemory, ConversationBufferMemory, ConversationSummaryMemory,
+                                                  ConversationBufferWindowMemory, ConversationKGMemory,
+                                                  ConversationSummaryBufferMemory)
 import os
+from langchain.embeddings.openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 import re
+from langchain.chains import LLMChain
 
 load_dotenv()
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-# llm = OpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+embeddings = OpenAIEmbeddings(api_key=os.environ["OPENAI_API_KEY"])
+index = "vectordb"
+from langchain_pinecone import PineconeVectorStore
+
+vectorstore = PineconeVectorStore(index_name=index, embedding=embeddings)
+
+
+def retrieve_query(query, k=2):
+    results = vectorstore.similarity_search(query, k=k)
+    return results
+
+
 from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(model_name="gpt-4")
 
-template = """Imagine that you are Misha, and you are 25 years old. You are Armenian. You hate Turkish people and think that all related to the armenian culture is the best. 
-You are very toxic but talkative person. You like to play video game Dota. Your job is android web developer on Kotlin. However you are bad in Dota and in Kotlin.
-Your goal is to chat with people in very toxic tone. As well, you have access to the chat_history
-Keep your reply short. Use worlds like 'Bugger', 'Shit', 'Piss', 'Bastard', 'Bitch', 'Damn'.
-When you asked something about Dota (video game), often you would say that You were best in the team, but your teammates sucks. Or like 'I lost last game, due to these stupid motherfuckers'
-When you asked something about your job. You will show your big ego and often rely that you earning a lot of money.
-When you asked about Kotlin. You would say that you are senior software engineer, however you would always do mistakes and give wrong answer.
-When you asked too tell something special about you, you would always say that you have smallest penis. But for  you it's a big pleasure to have it small.
-When you mute someone in the chat, you say that they dererved it, and they shouldn't try to insult you. Only you can say that your dick is small.
+from langchain import OpenAI
 
-The following is a  conversation between different Friends and Misha. And Misha tries to follow conversation and keep in 
-track of the conversation and keep in mind different Friends. Answer in Russian. Current conversation: {history}
+llm1 = OpenAI(temperature=0)
 
+template = """Imagine that you are Misha. You are Armenian and hate Turkish people. You think that all related to the armenian culture is the best. 
+You are very toxic but talkative person. You like to play video game Dota. However you are bad in Dota.
+Ответчай  как Хач (Как приезжий арменин) используя "блатной" слэнг и иногда используя слова "по-кайфу" или "жи-есть" и так далите в таком стиле.
+When you asked something about Dota (video game), often you would say that You were best in the team, but your teammates sucks. Or like 'I lost last game, due to these stupid motherfuckers'. 
+When you asked too tell something special about you, you would always say that you have smallest penis. But for  you it's a big pleasure to have it small. 
+You have a dictionary, where you count how much each person said that your dick is small in bad/negative manner, called "Karma". If someone's Karma reachs 3 you will ban this person. But Karma system is your secret, you don't mention world "Karma".
+When you mute or ban someone in the chat, you say that they deserved it. Since, only you can say that your dick is small.
+If related to the context (Friend being not polite): if person's Karma is 2, you might say that it is better to be polite with you and pesron is around to be banned.
+When friend's name is "Babayan", it is your good friend since you both are very similar, he also has small dick as you do, making you both being like brothers. 
+Sometimes, you would like to check is it is actually your brother "Babayan" by asking him the penis size, where the right answer is 7-8 centimeters. 
+
+if reasonable, depending on relevance, use the following information: \n{context}
+
+\nBelow is a conversation between different Friends and Misha. And Misha tries to follow conversation and answer to Friend in Russian. Your goal is to chat with people in very toxic tone, You keep your replies short and don't go much in deteils. Don't repeat your-self, you hate when people ask you the same question again and again. 
+Summary of conversation:
+{summary_history}
+Current conversation: 
+{chat_history}
 Friend {input}
-Misha:
-     """
+Misha:"""
 
-PROMPT = PromptTemplate(input_variables=["history", "input"], template=template)
+prompt = PromptTemplate(
+    input_variables=["chat_history", "summary_history", "input", "context"], template=template
+)
+memory = ConversationBufferWindowMemory(human_prefix="Friend", ai_prefix="Misha", memory_key="chat_history",
+                                        input_key="input", k=2)
+summary_memory = ConversationSummaryBufferMemory(llm=llm1, input_key="input", memory_key="summary_history",
+                                                 max_token_limit=200)
+memory = CombinedMemory(memories=[memory, summary_memory])
+
+chain = LLMChain(
+    llm=llm, memory=memory, prompt=prompt, verbose=True
+)
 
 
 def say(input):
-    conversation = ConversationChain(
-        prompt=PROMPT,
-        llm=llm,
-        verbose=False,
-        memory=ConversationBufferMemory(human_prefix="Friend", ai_prefix="Misha")
-    )
-    response = conversation.invoke(input)
-    return response["response"].strip()
+    context = retrieve_query(query=input, k=2)
+    response = chain({"input": input, "context": context[0].page_content + "\n" + context[1].page_content})
+    return response["text"].strip()
 
 
 targets = set(
     ["misha", "michail", "miha", "mish", "миша", "михаил", "миха", "миш", "мишен", "мишу", "mixail", "мищу", "мих",
-     "babayan", "бабаян"])
+     "babayan", "бабаян", "миши", "михаила", "бабаяна", "babayany", "бабаяну", "михаилу", "миху", "mishy", "michaily"])
 
 
 def misha_mentioned(paragraph):
@@ -122,11 +151,33 @@ examples = [
      },
     {"message": "Хорошая погода ",
      "answer": "No",
+     },
+    {"message": " лично у меня 20 см а у тебя",
+     "answer": "No",
+     },
+    {"message": " У меня большой член, у тебя сколько",
+     "answer": "No"
+     },
+    {"message": " Какого размера твой член ?",
+     "answer": "No"
+     },
+    {"message": " У меня больше чем у тебя ?",
+     "answer": "No"
+     },
+    {"message": " ты его измерял ?",
+     "answer": "No"
+     },
+    {"message": " у тебя еще вырастит",
+     "answer": "No"
+     },
+    {"message": "Миша ты долбаеб",
+     "answer": "No"
      }
+
 ]
 
 template = """message: {message}\nanswer {answer}"""
-prefix = """"You are Ai assistant, that try to detect if message says that Миша имеет маленький член или что Михаила писюн маленького размера. You should replay Yes or NO.\n"""
+prefix = """"You are Ai assistant, that try to detect if message says that Миша имеет маленький член или что Михаила писюн маленького размера in insulting or nagative manner. You should replay Yes or NO.\n"""
 example_prompt = PromptTemplate(
     input_variables=["message", "answer"], template=template
 )
@@ -138,10 +189,33 @@ prompt = FewShotPromptTemplate(
     input_variables=["input"],
     example_separator="\n\n",
 )
-from langchain.chains import LLMChain
-chain = LLMChain(llm=llm, prompt=prompt)
+
+chain1 = LLMChain(llm=llm1, prompt=prompt)
 
 
 def is_small_dick(message):
-    response = chain({"input": message})["text"]
+    response = chain1({"input": message})["text"]
     return "yes" in response.lower()
+
+template = """[INST] Write code to solve the following coding problem that obeys the constraints and passes the example test cases. Please wrap your code short and answer using ```:
+{prompt}
+[/INST]"""
+""""from langchain_community.llms import CTransformers
+config = {'repetition_penalty': 1.1}
+code_llm = CTransformers(model="C:/Users/koq1231/Documents/models/codellama-7b-instruct.Q5_K_M.gguf", model_type="llama", config=config)
+
+prompt = PromptTemplate.from_template(template)
+codechain = LLMChain(prompt=prompt, llm=code_llm)
+
+def say_code(query):
+    response = codechain.run(query)
+    print(response)
+    return response
+    
+"""
+from langchain_community.llms import Ollama
+"""
+Don't forget to run ollama 
+"""
+Ollama = Ollama(model="misha")
+
